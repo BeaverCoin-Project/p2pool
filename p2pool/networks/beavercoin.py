945013f18a3197dc2539ad617e08283b548d0cc2
nodecode <info@nodecode.de>
2014-07-11 17:18:02 +0200

    Create beavercoin.py

---
+++ p2pool/networks/beavercoin.py
@@ -0,0 +1,18 @@
+from p2pool.bitcoin import networks
+
+PARENT = networks.nets['beavercoin']
+SHARE_PERIOD = 15 # seconds
+CHAIN_LENGTH = 24*60*60//10 # shares
+REAL_CHAIN_LENGTH = 24*60*60//10 # shares
+TARGET_LOOKBEHIND = 200 # shares
+SPREAD = 3 # blocks
+IDENTIFIER = 'e037d5b8c6923410'.decode('hex')
+PREFIX = '7208c1a53ef629b0'.decode('hex')
+P2P_PORT = 2338
+MIN_TARGET = 0
+MAX_TARGET = 2**256//2**20 - 1
+PERSIST = True
+WORKER_PORT = 2332
+BOOTSTRAP_ADDRS = '37.120.160.23'.split(' ')
+ANNOUNCE_CHANNEL = '#p2pool-alt'
+VERSION_CHECK = lambda v: True
