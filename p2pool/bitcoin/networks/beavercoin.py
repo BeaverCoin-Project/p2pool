b21484f0ae83c01fd12c24ff3b0276d138ab5649
nodecode <info@nodecode.de>
2014-07-11 18:22:03 +0200

    Update beavercoin.py

--- p2pool/bitcoin/networks/beavercoin.py
+++ p2pool/bitcoin/networks/beavercoin.py
@@ -11,9 +11,9 @@ P2P_PREFIX = 'fbc0b6db'.decode('hex')
 P2P_PORT = 2333
 ADDRESS_VERSION = 48
 RPC_PORT = 2332
-RPC_CHECK = defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
-            'beavercoinaddress' in (yield bitcoind.rpc_help()) and
-            not (yield bitcoind.rpc_getinfo())['testnet']
+RPC_CHECK = defer.inlineCallbacks(lambda beavercoind: defer.returnValue(
+            'beavercoinaddress' in (yield beavercoind.rpc_help()) and
+            not (yield beavercoind.rpc_getinfo())['testnet']
         ))
 SUBSIDY_FUNC = lambda height: 50*100000000 >> (height + 1)//840000
 POW_FUNC = lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data))
