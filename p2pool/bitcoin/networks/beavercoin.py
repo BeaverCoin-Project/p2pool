f5bb6c0c22dbd7fb0163ff09b0720b83f49c2896
nodecode <info@nodecode.de>
2014-07-11 17:51:20 +0200

    Update beavercoin.py

--- p2pool/bitcoin/networks/beavercoin.py
+++ p2pool/bitcoin/networks/beavercoin.py
@@ -19,7 +19,7 @@ SUBSIDY_FUNC = lambda height: 50*100000000 >> (height + 1)//840000
 POW_FUNC = lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data))
 BLOCK_PERIOD = 60 # s
 SYMBOL = 'BVC'
-CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Litecoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Litecoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.litecoin'), 'litecoin.conf')
+CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'BeaverCoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/BeaverCoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.beavercoin'), 'beavercoin.conf')
 BLOCK_EXPLORER_URL_PREFIX = 'http://explorer.litecoin.net/block/'
 ADDRESS_EXPLORER_URL_PREFIX = 'http://explorer.litecoin.net/address/'
 TX_EXPLORER_URL_PREFIX = 'http://explorer.litecoin.net/tx/'
